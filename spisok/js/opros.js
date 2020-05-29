ec987f1df5c0cf0963696d9e75d84dd7119549ac
Evgeniy Vorobev <evggordadym@gmail.com>
2020-05-29 21:22:25 +0300

    Update opros.js

--- spisok/js/opros.js
+++ spisok/js/opros.js
@@ -151,6 +151,12 @@ $(function () {
 
 })
 
+/*выводим в верзхний слой меню, уменьшаем z-index у мешающих элементов*/
+$(function () {
+    $('[data-elem-type="image"').css({"z-index" : '10'})
+    $('[data-elem-type="text"').css({"z-index" : '10'})
+})
+
 // Кастомная функция для удаление всех кукисов
 function removeCookies() {
     var cookies = $.cookie();
